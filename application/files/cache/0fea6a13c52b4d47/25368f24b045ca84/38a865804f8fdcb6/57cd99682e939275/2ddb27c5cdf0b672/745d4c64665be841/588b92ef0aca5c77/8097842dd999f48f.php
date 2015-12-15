291b333b0907023ace8c04b4a2518cd517ddacd7
yakamoz <qj.fang@163.com>
2015-12-15 23:30:38 +0800

    框架原始代码

---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/588b92ef0aca5c77/8097842dd999f48f.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_107d2673e16fa399ff1eb77db045fc73_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1450498674;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1450108643;
